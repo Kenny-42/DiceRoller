23eb8072be68f1e5af18d405c350d05c3ebeddd6
Kendall Dawson <lilkenny642@gmail.com>
2025-07-18 03:18:38 -0700

    Add FaceUpValue property and Roll method to Die class
    Enhance the Die class by introducing a new property, FaceUpValue, to track the current face-up value. Implement the Roll method to simulate rolling the die, which randomly sets the face-up value based on the number of sides.

--- DiceRoller/Die.cs
+++ DiceRoller/Die.cs
@@ -26,7 +26,7 @@ public class Die
             {
                 throw new ArgumentOutOfRangeException(nameof(numSides)
                     , $"{nameof(numSides)} must be greater than 0 and less than 21");
-        }
+            }
             NumberOfSides = numSides;
         }
 
@@ -34,4 +34,28 @@ public class Die
     /// Gets the number of sides of the die.
     /// </summary>
     public byte NumberOfSides { get; private set; }
+
+    /// <summary>
+    /// The current face up value for the die.
+    /// </summary>
+    public byte FaceUpValue { get; private set; }
+
+    /// <summary>
+    /// Simulates rolling the die and returns the resulting face up value.
+    /// </summary>
+    /// <remarks>
+    /// The face-up value is randomly determined based on the number of sides of the die.
+    /// The value is updated internally and can be retrieved using the <see cref="FaceUpValue"/> property.
+    /// </remarks>
+    /// <returns>
+    /// A byte representing the face-up value of the die after the roll. The value
+    /// will be between 1 and the number of sides of the die, inclusive.
+    /// </returns>
+    public byte Roll()
+    {
+        Random rand = new();
+        FaceUpValue = Convert.ToByte(rand.Next(1, NumberOfSides + 1));
+
+        return FaceUpValue;
     }
+}
