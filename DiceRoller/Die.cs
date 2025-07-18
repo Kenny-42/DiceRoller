14091cf1c889606e1bd5e8685910fbd6ebfd0616
Kendall Dawson <lilkenny642@gmail.com>
2025-07-18 03:34:23 -0700

    Add comment to clarify die initialization behavior
    Added a comment in the `Die` class to indicate that the die will start as a random number after setting the number of sides. This comment precedes the call to the `Roll()` method, which initializes the die's value.

--- DiceRoller/Die.cs
+++ DiceRoller/Die.cs
@@ -30,6 +30,9 @@ public class Die
                 , $"{nameof(numSides)} must be greater than 0 and less than 21");
         }
         NumberOfSides = numSides;
+
+        // Forces the die to start as a random number
+        Roll();
     }
 
     /// <summary>
